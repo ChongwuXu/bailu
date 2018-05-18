f39fa54a20d87bd8cc51f26dff32d4f805cf789a
xuchongwu <xuchongwu@hoolai.com>
2018-05-18 19:52:55 +0800

    白鹭

--- frame/src/Main.ts
+++ frame/src/Main.ts
@@ -33,18 +33,18 @@ class Main extends egret.DisplayObjectContainer {
 
     public constructor() {
         super();
-          let ob = {
-            token: "1234567",
-            openId: "ssss",
-            player: {
-                account: "1234567",
-                nickname: "nihaoha"
-            }
-        }
-        let w = game.json.GameLoginReq.encode(<any>ob);
-        let r = game.json.GameLoginReq.decode(w.finish());
-        console.log("--------------------------->>>>" + JSON.stringify(r.toJSON()));
-        //this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+        //   let ob = {
+        //     token: "1234567",
+        //     openId: "ssss",
+        //     player: {
+        //         account: "1234567",
+        //         nickname: "nihaoha"
+        //     }
+        // }
+        // let w = game.json.GameLoginReq.encode(<any>ob);
+        // let r = game.json.GameLoginReq.decode(w.finish());
+        // console.log("--------------------------->>>>" + JSON.stringify(r.toJSON()));
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
     }
 
     private onAddToStage(event: egret.Event) {
